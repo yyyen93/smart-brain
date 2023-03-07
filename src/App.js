a194a732c34ec08f4d7f2b440e6137b5f467fa03
yyyen93 <37173505+yyyen93@users.noreply.github.com>
2023-03-07 14:30:15 +0800

    Added server link in the fetch to Register.js Signin.Js smart component

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ class App extends Component{
   onPictureSubmit = () => {
     this.setState({imageUrl: this.state.input});
     //fetch handleApiCall / imageurl
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://smartbrain-1zip.onrender.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -116,7 +116,7 @@ class App extends Component{
     .then(response => response.json())
     .then(response => {
       if(response){
-        fetch('http://localhost:3000/image', {
+        fetch('https://smartbrain-1zip.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
